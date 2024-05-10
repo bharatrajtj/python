b9d1d7f901e0f9046e657bada77e5c3e914e4bcc
bharatrajtj <65009556+bharatrajtj@users.noreply.github.com>
2024-05-10 09:04:55 -0400

    Create functions.py

---
+++ day2/functions.py
@@ -0,0 +1,23 @@
+### Gloabal Variable
+
+a = 10
+b = 4
+
+
+def add():
+    add = a + b
+    print(add)
+
+def sub():
+    sub = a - b
+    print(sub)
+
+def mul():
+    mul = a * b
+    print(mul)
+
+#### Need to explicitly call functions, to get executed and generate output
+
+add()
+sub()
+mul()
