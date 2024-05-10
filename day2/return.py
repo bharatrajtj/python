1ef4a88fe219408b6ad7fc9850e5e825129c5e1f
bharatrajtj <65009556+bharatrajtj@users.noreply.github.com>
2024-05-10 10:33:00 -0400

    Create return.py

---
+++ day2/return.py
@@ -0,0 +1,16 @@
+def add(a,b):       ###Function takes Input a,b
+    add = a + b     ###Performs logic
+    return add      ### Returns output to the function
+
+def sub(a,b):
+    sub = a - b
+    return sub
+
+def mul(a,b):
+    mul = a * b
+    return mul
+
+
+print(add(7,5))   ### Return keyword replaces value for add(7,5)
+print(sub(100,48))
+print(mul(3,97))
