8ee1f8d625c6e6f9c76d9b3b4e63c74fd74c013d
bharatrajtj <65009556+bharatrajtj@users.noreply.github.com>
2024-05-10 09:17:24 -0400

    Create adv_functions.py

---
+++ day2/adv_functions.py
@@ -0,0 +1,8 @@
+##Modules_1
+
+
+import functions as calc   ###Importing functions.py as module
+
+calc.add()
+
+###Genrate all the function defined in functions.py and will generate one more output of add function
