6820d3bcdde75d8907863cec9819063b8245d628
bharatrajtj <65009556+bharatrajtj@users.noreply.github.com>
2024-05-19 10:40:17 -0400

    Create loops.py

---
+++ day2/loops.py
@@ -0,0 +1,9 @@
+### For loop is used when we have Definite value for the number of types loop to be repeated
+for a in range(19):
+    print(a)
+
+#### For loop with list
+
+country = ["India", "Canada", "USA", "Scotland"]
+for a in country:        #### IN takes the element of list Country and store in variable
+    print(a)
